fdf98be7c3dfa4ef079073025c5a6cf6ccb055c8
Pooria Rahimi <pooriarahimi159@gmail.com>
2024-08-29 21:31:59 +0330

    fix setting file

--- digital_products/settings.py
+++ digital_products/settings.py
@@ -122,7 +122,7 @@ STATIC_URL = 'static/'
 
 # Media Files
 MEDIA_ROOT = os.path.join(BASE_DIR , 'media/')
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
